ab08daec50a7d57acb6afb7ac9057f1fdd57f285
poshaw <poadshaw@gmail.com>
2017-12-14 09:42:35 -0600

    added content to main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var number1 = 35;
+var number2 = 40;
+var number3 = number1 + number2;
+var string1 = "hi"
+console.log(string1 + " " + number3);
+var colors = ['red', 'white', 'blue'];
+colors.push('purple');
+console.log(colors[3]);
+for(var i = 0; i < 4; i++){
+	console.log(i);
+}
